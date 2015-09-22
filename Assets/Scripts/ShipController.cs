bc44d30399cf93e13b75df98681bcade7064c25d
piersy <pierspowlesland@gmail.com>
2015-09-22 23:00:57 +0100

    Addition of bezier curve, need to orient segments to the bezier derivative

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -12,8 +12,13 @@ public class ShipController : MonoBehaviour
     private GameObject controlMeshGameObject;
     Vector3[] newVertices;
     GlLineRenderer lineRenderer;
+    GameObject go;
+    GameObject go2;
     void Start()
     {
+
+        go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
         Vector3 front = frontOfShip();
         controlCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         //ControlCube has its postion set to 10 units in front of the ship
@@ -22,21 +27,21 @@ public class ShipController : MonoBehaviour
         controlCube.AddComponent<Drag>();
 
         controlMeshGameObject = new GameObject();
-        lineRenderer  = new GlLineRenderer(newMaterial());
+        lineRenderer = new GlLineRenderer(newMaterial());
     }
 
     private Material newMaterial()
     {
-          Material  lineMaterial = new Material("Shader \"Lines/Colored Blended\" {" +
-                "SubShader { Pass { " +
-                "    Blend SrcAlpha OneMinusSrcAlpha " +
-                "    ZWrite Off Cull Off Fog { Mode Off } " +
-                "    BindChannels {" +
-                "      Bind \"vertex\", vertex Bind \"color\", color }" +
-                "} } }");
-            lineMaterial.hideFlags = HideFlags.HideAndDontSave;
-            lineMaterial.shader.hideFlags = HideFlags.HideAndDontSave;
-            return lineMaterial;
+        Material lineMaterial = new Material("Shader \"Lines/Colored Blended\" {" +
+                                       "SubShader { Pass { " +
+                                       "    Blend SrcAlpha OneMinusSrcAlpha " +
+                                       "    ZWrite Off Cull Off Fog { Mode Off } " +
+                                       "    BindChannels {" +
+                                       "      Bind \"vertex\", vertex Bind \"color\", color }" +
+                                       "} } }");
+        lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+        lineMaterial.shader.hideFlags = HideFlags.HideAndDontSave;
+        return lineMaterial;
     }
     //Front of ship, the epoint from which the controll mesh starts
     Vector3 frontOfShip()
@@ -50,8 +55,16 @@ public class ShipController : MonoBehaviour
         //We draw our control mesh segments with a distance of 1 between them
         //So we truncate the distance to be an integer value to simplify segment placement
         int intDistance = (int)Vector3.Distance(controlCube.transform.position, front);
+        //The bezier controll point straight out in front of the ship
+        Vector3 curveControllPoint = new Vector3(front.x, front.y + controlCube.transform.position.y, front.z);
+
+        go.transform.position = curveControllPoint;
+        go.GetComponent<MeshRenderer>().material.color = Color.white;
         //the direction from the ship to the control cube
         Vector3 direction = (controlCube.transform.position - front).normalized;
+        Vector3 endPoint = front + ((intDistance-1) * direction);
+        go2.transform.position = endPoint;
+        go2.GetComponent<MeshRenderer>().material.color = Color.white;
         //set the control cube to follow this direction
         controlCube.transform.rotation = Quaternion.LookRotation(direction);
         //control mesh verticies one segment per  unit distance
@@ -59,19 +72,24 @@ public class ShipController : MonoBehaviour
         Vector2[] newUV;
         int vOffset = 0;
         int prevOffset;
-        linesList = new List<int>();
+        linesList = new List<int>(4 * intDistance * 2 + 4 * (intDistance - 1) * 2);
         Vector3 segmentPosition = front;
         //Attempting to create a mesh object, to replace the existing object indicator
         for (int i = 0; i < intDistance; i++)
         {
             prevOffset = vOffset;
             vOffset = i * 4;
-            
-            segmentPosition = front + (i * direction);
-            newVertices[vOffset + 0] = segmentPosition + controlCube.transform.up + controlCube.transform.right;
-            newVertices[vOffset + 1] = segmentPosition - controlCube.transform.up + controlCube.transform.right;
-            newVertices[vOffset + 2] = segmentPosition - controlCube.transform.up - controlCube.transform.right;
-            newVertices[vOffset + 3] = segmentPosition + controlCube.transform.up - controlCube.transform.right;
+            //segmentPosition = front + (i * direction);
+         
+            float t = (float)i/(float)intDistance;
+            Vector3 bezierPoint = (Mathf.Pow(1f-t,2) * front) + (2*(1f-t)*t*curveControllPoint) + (Mathf.Pow(t,2) * endPoint); 
+            Vector3 bezierSlope  = 2*(1f-t)*(curveControllPoint - front) + 2*t*(endPoint -curveControllPoint);
+
+            newVertices[vOffset + 0] = bezierPoint + controlCube.transform.up + controlCube.transform.right;
+            newVertices[vOffset + 1] = bezierPoint - controlCube.transform.up + controlCube.transform.right;
+            newVertices[vOffset + 2] = bezierPoint - controlCube.transform.up - controlCube.transform.right;
+            newVertices[vOffset + 3] = bezierPoint + controlCube.transform.up - controlCube.transform.right;
+
             //Add lines connecting each segment
             linesList.Add(vOffset + 0);
             linesList.Add(vOffset + 1);
