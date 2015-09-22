692451e8fd6526de88f9ee178c6dbd88092689d4
piersy <pierspowlesland@gmail.com>
2015-09-22 21:15:11 +0100

    Lots of cleanup code working well

---
+++ Assets/Scripts/GlLineRenderer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlLineRenderer
+{
+    
+    private Material material;
+
+    public GlLineRenderer(Material material)
+    {
+        this.material = material;
+    }
+    
+    public void renderLines(Vector3[] verts, int[] indexes)
+    {
+        material.SetPass(0);
+        GL.PushMatrix();
+        //GL.MultMatrix(controlMeshGameObject.transform.localToWorldMatrix);
+        GL.Begin(GL.LINES);
+        GL.Color(new Color(1, 1, 1, 0.1f));
+        for (int i = 0; i <indexes.Length; i++)
+        {
+            GL.Vertex(verts[indexes[i]]);
+        }
+        GL.End();
+        GL.PopMatrix();
+    }
+}
--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,24 +4,16 @@ using System.Collections.Generic;
 
 public class ShipController : MonoBehaviour
 {
+    //The list that holds all the lines
     List<int> linesList;
     //Cube that sits in front of the ship and can be dragged by the player in order to control the ship
     private GameObject controlCube;
-    //Wireframe mesh that helps indicate the control applied to the ship
+    //Game object that is used to help construct the control mesh
     private GameObject controlMeshGameObject;
-    private Mesh controlMesh;
-    //The material used to render the wireframe of the controlmesh
-    private Material lineMaterial;
-    Color lineColor = Color.red;
-    Color backgroundColor = Color.black;
-    bool ZWrite = true;
-    bool AWrite = true;
-    bool blend = true;
-    //The indexes of al the lines used to draw the control mesh
+    Vector3[] newVertices;
+    GlLineRenderer lineRenderer;
     void Start()
     {
-        Debug.Log("Starring ship");
-
         Vector3 front = frontOfShip();
         controlCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         //ControlCube has its postion set to 10 units in front of the ship
@@ -29,28 +21,34 @@ public class ShipController : MonoBehaviour
         controlCube.GetComponent<MeshRenderer>().material.color = Color.red;
         controlCube.AddComponent<Drag>();
 
-        Vector3[] verts = controlCube.GetComponent<MeshFilter>().mesh.vertices;
         controlMeshGameObject = new GameObject();
-        MeshFilter meshFilter = controlMeshGameObject.AddComponent<MeshFilter>();
-        controlMesh = controlMeshGameObject.GetComponent<MeshFilter>().mesh;
-        controlMeshGameObject.AddComponent<MeshRenderer>(); 
+        lineRenderer  = new GlLineRenderer(newMaterial());
     }
 
+    private Material newMaterial()
+    {
+          Material  lineMaterial = new Material("Shader \"Lines/Colored Blended\" {" +
+                "SubShader { Pass { " +
+                "    Blend SrcAlpha OneMinusSrcAlpha " +
+                "    ZWrite Off Cull Off Fog { Mode Off } " +
+                "    BindChannels {" +
+                "      Bind \"vertex\", vertex Bind \"color\", color }" +
+                "} } }");
+            lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+            lineMaterial.shader.hideFlags = HideFlags.HideAndDontSave;
+            return lineMaterial;
+    }
     //Front of ship, the epoint from which the controll mesh starts
     Vector3 frontOfShip()
     {
         return new Vector3(transform.position.x, transform.position.y + GetComponent<Renderer>().bounds.extents.y, transform.position.z);
     }
 
-    private Vector3 screenPoint;
-    private Vector3 offset;
-
-    Vector3[] newVertices;
     void LateUpdate()
     {
         Vector3 front = frontOfShip();
         //We draw our control mesh segments with a distance of 1 between them
-        //So we truncate the distance to be an inteer value to simplify segment placement
+        //So we truncate the distance to be an integer value to simplify segment placement
         int intDistance = (int)Vector3.Distance(controlCube.transform.position, front);
         //the direction from the ship to the control cube
         Vector3 direction = (controlCube.transform.position - front).normalized;
@@ -59,41 +57,21 @@ public class ShipController : MonoBehaviour
         //control mesh verticies one segment per  unit distance
         newVertices = new Vector3[4 * intDistance];
         Vector2[] newUV;
-        //control mesh triangles we will have 2 triangles per face and distance-1 * 4 face
-        //a triangle is made up of 3 indexes 
-        int[] newTriangles = new int[intDistance * 4 * 2 * 3];
-        //we precalculate the first set of verts here so that we can allways set triangles in the loop
-        Vector3 segPos = front;
-        newVertices[0] = segPos + controlCube.transform.up + controlCube.transform.right;
-        newVertices[1] = segPos - controlCube.transform.up + controlCube.transform.right;
-        newVertices[2] = segPos - controlCube.transform.up - controlCube.transform.right;
-        newVertices[3] = segPos + controlCube.transform.up - controlCube.transform.right;
         int vOffset = 0;
         int prevOffset;
-        //we want four lines for each segment and then four lines to connect each segment to the next
         linesList = new List<int>();
-        linesList.Add(0);
-        linesList.Add(1);
-
-        linesList.Add(1);
-        linesList.Add(2);
-            
-        linesList.Add(2);
-        linesList.Add(3);
-            
-        linesList.Add(3);
-        linesList.Add(0);
+        Vector3 segmentPosition = front;
         //Attempting to create a mesh object, to replace the existing object indicator
-        for (int i = 1; i < intDistance; i++)
+        for (int i = 0; i < intDistance; i++)
         {
             prevOffset = vOffset;
             vOffset = i * 4;
             
-            segPos = front + (i * direction);
-            newVertices[vOffset + 0] = segPos + controlCube.transform.up + controlCube.transform.right;
-            newVertices[vOffset + 1] = segPos - controlCube.transform.up + controlCube.transform.right;
-            newVertices[vOffset + 2] = segPos - controlCube.transform.up - controlCube.transform.right;
-            newVertices[vOffset + 3] = segPos + controlCube.transform.up - controlCube.transform.right;
+            segmentPosition = front + (i * direction);
+            newVertices[vOffset + 0] = segmentPosition + controlCube.transform.up + controlCube.transform.right;
+            newVertices[vOffset + 1] = segmentPosition - controlCube.transform.up + controlCube.transform.right;
+            newVertices[vOffset + 2] = segmentPosition - controlCube.transform.up - controlCube.transform.right;
+            newVertices[vOffset + 3] = segmentPosition + controlCube.transform.up - controlCube.transform.right;
             //Add lines connecting each segment
             linesList.Add(vOffset + 0);
             linesList.Add(vOffset + 1);
@@ -122,61 +100,12 @@ public class ShipController : MonoBehaviour
 
         }
 		
-        drawThisThing();
     }
-
-    void setQuadTris(int index, int[] tris, int topRight, int bottomRight, int topLeft, int bottomLeft)
-    {
-        setTri(index, tris, topRight, topLeft, bottomRight);
-        setTri(index + 3, tris, bottomRight, bottomLeft, topLeft);
-    }
-
-    void setTri(int index, int[] tris, int corner1, int corner2, int corner3)
-    {
-        tris[index + 0] = corner1;
-        tris[index + 1] = corner2;
-        tris[index + 2] = corner3;
-    }
-
-    private void drawThisThing()
-    {
-    
-        controlMeshGameObject.GetComponent<MeshRenderer>().material = new Material("Shader \"Lines/Background\" { Properties { _Color (\"Main Color\", Color) = (1,1,1,1) } SubShader { Pass {" + (ZWrite ? " ZWrite on " : " ZWrite off ") + (blend ? " Blend SrcAlpha OneMinusSrcAlpha" : " ") + (AWrite ? " Colormask RGBA " : " ") + "Lighting Off Offset 1, 1 Color[_Color] }}}");
-  
-        // Old Syntax without Bind :    
-        //   lineMaterial = new Material("Shader \"Lines/Colored Blended\" { SubShader { Pass { Blend SrcAlpha OneMinusSrcAlpha ZWrite On Cull Front Fog { Mode Off } } } }"); 
-  
-        // New Syntax with Bind : 
-        lineMaterial = new Material("Shader \"Lines/Colored Blended\" { SubShader { Pass { Blend SrcAlpha OneMinusSrcAlpha BindChannels { Bind \"Color\",color } ZWrite On Cull Front Fog { Mode Off } } } }");    
-  
-        lineMaterial.hideFlags = HideFlags.HideAndDontSave;
-        lineMaterial.shader.hideFlags = HideFlags.HideAndDontSave;
-  
-        List<Vector3> linesArray = new List<Vector3>();
-        MeshFilter filter = GetComponent<MeshFilter>();
-        Mesh mesh = filter.sharedMesh;
-    }
-	
 
     void  OnRenderObject()
     {    
-        MeshRenderer meshRenderer = controlMeshGameObject.GetComponent<MeshRenderer>();
-        meshRenderer.sharedMaterial.color = backgroundColor; 
-        lineMaterial.SetPass(0); 
-             
-        GL.PushMatrix(); 
-        GL.MultMatrix(controlMeshGameObject.transform.localToWorldMatrix); 
-        GL.Begin(GL.LINES); 
-        GL.Color(lineColor); 
-             
-        Debug.Log(linesList.Count);
-        for (int i = 0; i < linesList.Count; i++)
-        { 
-            GL.Vertex(newVertices[linesList[i]]); 
-        } 
-             
-        GL.End(); 
-        GL.PopMatrix(); 
+        lineRenderer.renderLines(newVertices, linesList.ToArray());
     }
+
 
 }
