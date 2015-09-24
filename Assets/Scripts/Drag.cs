5aef3e240ba4aca53091461d3b075e23cc893c31
piersy <pierspowlesland@gmail.com>
2015-09-24 23:03:13 +0100

    Added camera rotation around the object with centering on click

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -5,25 +5,22 @@
 public class Drag : MonoBehaviour
 {
 	
-	private Vector3 screenPoint;
-	private Vector3 offset;
+    private Vector3 screenPoint;
+    private Vector3 offset;
 
-	void OnMouseDrag ()
-	{
-		Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		Vector3 curPosition =Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
-		transform.position = curPosition;
-	}
-		
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
 
-    void OnMouseDown ()
-          {
-                  screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
   
-                  offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
   
-          }
-
-	
+    }
 	
 }
