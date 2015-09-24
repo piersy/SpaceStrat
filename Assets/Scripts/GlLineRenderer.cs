0ae48b7c9bf296278981ef3f74abc63acd0f650d
piersy <pierspowlesland@gmail.com>
2015-09-24 20:00:20 +0100

    Selected colors for control mesh rendering yellow for near and darkish red for far

--- Assets/Scripts/GlLineRenderer.cs
+++ Assets/Scripts/GlLineRenderer.cs
@@ -19,7 +19,10 @@ public class GlLineRenderer
         GL.Begin(GL.LINES);
         for (int i = 0; i < indexes.Length; i++)
         {
-            GL.Color(new Color(1, 1, 1, 1f-(distances[indexes[i]])));
+            float d = distances[indexes[i]];
+            // yellow fading to purple GL.Color(new Color(1f-(d/2f), 1f-d, d, 0.5f));
+            //Yellow fading to darkish red
+            GL.Color(new Color(1f-(d/2f), 1f-d, 0.2f, 0.5f));
             GL.Vertex(verts[indexes[i]]);
         }
         GL.End();
