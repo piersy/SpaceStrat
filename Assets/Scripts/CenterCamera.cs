63632e5e287e65aed6e98759fa89888470073f3f
piersy <pierspowlesland@gmail.com>
2015-09-25 09:21:46 +0100

    Addning camera scripts, not working with the cnon convex mesh + rigid body

---
+++ Assets/Scripts/CenterCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshCollider))]
+
+public class CenterCamera : MonoBehaviour
+{
+    
+    void OnMouseDown()
+    {
+        Camera.main.transform.LookAt(transform.position, Camera.main.transform.up);
+    }
+
+
+
+    void OnMouseDrag()
+    {
+        float x = Input.GetAxis("Mouse X"); 
+        float y = Input.GetAxis("Mouse Y");
+        Camera.main.transform.RotateAround(transform.position, Camera.main.transform.up, x*5);
+        Camera.main.transform.RotateAround(transform.position, Camera.main.transform.right, y*5);
+        //Camera.main.transform.RotateAround(transform.position, transform.up, x);
+    }
+
+
+}
